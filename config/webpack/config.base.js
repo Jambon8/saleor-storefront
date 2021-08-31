0774d2cd1b0ac437e18383824b1651aaafc297a7
Jambon8 <42007262+Jambon8@users.noreply.github.com>
2021-08-31 22:16:01 +0400

    Update config.base.js

--- config/webpack/config.base.js
+++ config/webpack/config.base.js
@@ -102,8 +102,8 @@ module.exports = ({ sourceDir, distDir }) => ({
       logo: `${sourceDir}/images/favicon.png`,
       prefix: "images/favicons/",
       favicons: {
-        appName: "Saleor",
-        appDescription: "Storefront for the Saleor e-commerce platform",
+        appName: "Mia Mart",
+        appDescription: "Jewellery online shop",
         display: "standalone",
         developerURL: null, // prevent retrieving from the nearest package.json
         background: "#ddd",
